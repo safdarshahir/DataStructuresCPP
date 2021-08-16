2ecf3a276b292de25fff442d4a304b4c1e3d340e
safdarshahir <safdarshahir@gmail.com>
2021-08-16 13:56:08 +0530

    Added postfix evalutaion

---
+++ Stack/6-postfixEval.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+bool isoperator(char c){
+
+    if(c == '+' || c == '-' || c == '/'|| c == '*'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int eval(int op1, int op2,char c){
+
+    switch(c){
+        case '+': return op2+op1; 
+        case '-': return op2-op1;
+        case '/': return op2/op1;
+        case '*': return op2*op1;
+    
+    }
+} 
+
+int posteval(string s){
+
+    stack<char> Stack;
+
+    for(int i = 0; i < s.length();i++){
+
+        if(!isoperator(s[i])){
+            Stack.push((s[i]-'0'));
+            continue;
+        }
+        int op1 = Stack.top();
+        Stack.pop();
+        int op2 = Stack.top();
+        Stack.pop();
+        Stack.push(eval(op1,op2,s[i]));
+
+    }
+
+    return Stack.top();
+
+}
+
+int main(){
+
+    string str;
+
+    cout<<"\nEnter postfix Expression:";
+    getline(cin,str);
+
+    cout<<"\nPostfix evaluation:"<<posteval(str);
+
+    return 0;
+    
+}
